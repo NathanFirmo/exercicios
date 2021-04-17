a88ac0c55e5df431652f32c662e0fcc602ab7b85
Nathan Firmo <nathanssfirmo@gmail.com>
2021-04-17 19:48:14 -0300

    :sparkles: creating my first node server

---
+++ Node.js/1-beginner/first-server/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res){ /*A barra significa a rota, nesse caso, a página principal*/
+    res.render("index")
+})
+
+app.get("/sobre", function(req, res){
+    res.render("about")
+})
+
+app.listen(8080)
+console.log('Run')
